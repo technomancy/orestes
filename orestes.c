4bb0d71aef450577178293cdb04e09a3f2bf73b4
Phil Hagelberg <technomancy@gmail.com>
2014-03-28 19:22:49 -0700

    Use null for string terminators instead of pascal-style sizes.

--- orestes.c
+++ orestes.c
@@ -6,6 +6,7 @@
 
 #define error printf
 #define db printf
+#define out printf
 
 typedef int cell;
 
@@ -17,15 +18,12 @@ enum entry_type {
   COLON
 };
 
-struct dictionary {
-  struct dictionary * prev;
-  char name_size;
-  char * name;
+typedef struct {
+  struct dict * prev;
   enum entry_type type;
+  char * name;
   cell * body;
-};
-
-typedef struct dictionary dict;
+} dict;
 
 cell * s0 = NULL;
 cell * sp = NULL;
@@ -36,11 +34,11 @@ dict * compiling = NULL;
 cell * ip = NULL;
 cell * dp = NULL;
 
-char skipping = 0;
-
 char * tib = \
   "0 : hi if 12 32 + then 11 ; hi .s "; // should be 11
   // "9 dup constant x variable y : inc 1 + ; 3211 y ! x + y @ + inc .s "; // 3230
+
+char skipping = 0;
 
 
 // helper functions
@@ -51,14 +49,13 @@ void push(cell c) {
   sp++;
 };
 
-void define(char * name, char name_size, enum entry_type type, void * body) {
+void define(char * name, enum entry_type type, void * body) {
   dict * cp_prev = cp;
 
   cp = (dict*)malloc(sizeof(dict));
 
   if(cp != NULL) {
     cp->prev = cp_prev;
-    cp->name_size = name_size;
     cp->name = name;
     cp->body = (cell*) body;
     cp->type = type;
@@ -67,30 +64,19 @@ void define(char * name, char name_size, enum entry_type type, void * body) {
   }
 };
 
-void define_primitive(char * name, void (*body)(void)) {
-  int length = 0;
-  for(length = 0; name[length]; length++) {}
-  define(name, length, PRIMITIVE, body);
-};
-
 char check_for_done_skipping() {
   dict * entry = drop();
 
-  db("entry %d\n", entry);
   if(entry && entry->type == PRIMITIVE) {
     // literals need an extra skip
     push(entry->name);
-    push(entry->name_size);
     push("literal");
-    push(7);
     string_eq();
     if(drop()) {
       ip++;
     }
     push(entry->name);
-    push(entry->name_size);
     push("then");
-    push(4);
     string_eq();
     return !drop();
   } else {
@@ -117,21 +103,12 @@ void dup(void) {
   push(c);
 };
 
-void dup2(void) {
-  cell c1 = drop();
-  cell c2 = drop();
-  push(c2);
-  push(c1);
-  push(c2);
-  push(c1);
-};
-
 void dot_s(void) {
-  printf("<%d> ", (sp - s0) / sizeof(cell));
+  out("<%d> ", sp - s0);
   for(cell * i = s0; i < sp; i++) {
-    printf("%d ", *i);
+    out("%d ", *i);
   }
-  printf("\n");
+  out("\n");
 };
 
 void fetch(void) {
@@ -154,53 +131,57 @@ void plus(void) {
 // parsing primitives
 
 void to_number(void) { // c-addr1 u1 -- ud2 f
-  cell in_size = drop();
-  char * in = (char*) drop();
+  char * in = (char*)drop();
   cell n = 0;
 
-  for(char * i = in; i < in_size + in; i++) {
-    if(*i >= 48 && *i < 58) {
+  for(char i = 0; in[i]; i++) {
+    if(in[i] >= 48 && in[i] < 58) {
       n *= 10;
-      n += (*i - 48);
+      n += (in[i] - 48);
     } else {
+      free(in);
       push(0);
       return;
     }
   }
 
+  free(in);
   push(n);
   push(1);
 };
 
-void word(void) { // char -- c-addr u
-  cell delimiter = drop();
-  char * i = 0;
+void word(void) {
+  char i = 0;
+  char * new_str = 0;
 
-  // TODO: need to allocate space for this once tib gets replaced
-  for(i = tib; *i != (char)delimiter; i++) {}
+  // skip till space or newline or null
+  while(tib[i] != 32 && tib[i] != 10 && tib[i]) { i++; };
+  i++;
 
-  push(tib);
-  push(i - tib);
-  tib = i + 1;
+  if(new_str = malloc(i)) {
+    for(char j = 0; j < i - 1; j++) {
+      new_str[j] = tib[j];
+    }
+    new_str[i] = 0; // slap a null terminator on
+
+    push(new_str);
+    tib += i;
+  } else {
+    error("oom\n");
+  }
 };
 
-void string_eq(void) { // c1-addr u1 c2-addr u2 -- f
-  int c1_size = (int)drop();
+void string_eq(void) {
   char * c1 = (char*)drop();
-  int c2_size = (int)drop();
-  char * c2   = (char*)drop();
+  char * c2 = (char*)drop();
 
-  if(c1_size != c2_size) {
-    push(0);
-  } else {
-    for(int i = 0; i < c1_size; i++) {
-      if(*(c1 + i) != *(c2 + i)) {
-        push(0);
-        return;
-      }
+  for(int i = 0; c1[i] || c2[i]; i++) {
+    if(c1[i] != c2[i]) {
+      push(0);
+      return;
     }
-    push(1);
   }
+  push(1);
 };
 
 
@@ -218,16 +199,12 @@ void then(void) {
 // interpreter and compiler primitives
 
 void find(void) {
-  cell target_size = drop();
   cell target = drop();
 
   for(dict * cur = cp; cur; cur = cur->prev) {
     push(target);
-    push(target_size);
     push(cur->name);
-    push(cur->name_size);
     string_eq();
-
     if(drop()) {
       push(cur);
       return;
@@ -238,34 +215,26 @@ void find(void) {
 
 void literal(void) {
   ip++;
-  db("lit\n");
   push(*ip);
-  db("plit\n");
 };
 
 void constant(void) {
   cell value = drop();
-  push((cell)32);
   word();
-  char name_size = (char)drop();
   char * name = (char*)drop();
-  define(name, name_size, CONSTANT, value);
+  define(name, CONSTANT, value);
 };
 
 void variable(void) {
-  push((cell)32);
   word();
-  char name_size = (char)drop();
   char * name = (char*)drop();
-  define(name, name_size, VARIABLE, -1);
+  define(name, VARIABLE, -1);
 };
 
 void colon(void) {
-  push((cell)32);
   word();
 
   compiling = (dict*)malloc(sizeof(dict));
-  compiling->name_size = (char)drop();
   compiling->name = (char*)drop();
   compiling->body = malloc(MAX_WORD_SIZE);
   compiling->type = COLON;
@@ -297,16 +266,16 @@ void execute(void) {
     push(entry);
     add_to_definition();
   } else if(entry->type == PRIMITIVE || entry->type == IMMEDIATE) {
-    db(" > %s\n", entry->name);
+    // db(" p> %s\n", entry->name);
     void (*primitive)(void) = entry->body;
     (*primitive)();
   } else if(entry->type == COLON) {
     for(ip = entry->body; *ip; ip++) {
-      db("docolon %d %d\n", ip, skipping);
       push(*ip);
       if(skipping) {
         skipping = check_for_done_skipping();
       } else {
+        // db(" :> %s\n", entry->name);
         execute();
       }
     }
@@ -321,27 +290,27 @@ void execute(void) {
 
 void interpret(void) {
   // dot_s();
-  push((cell)32);
   word();
 
-  dup2(); // keep it around in case it's a number
+  dup(); // keep it around in case it's a number
   find();
-
   dict * entry = drop();
 
   if(entry) {
-    drop(); drop(); // drop dup'd word for to_number
+    free((char *)drop());
     push(entry);
     execute();
   } else {
     if(compiling) {
       to_number();
-      drop();
-      push("literal");
-      push(7);
-      find();
-      add_to_definition();
-      add_to_definition();
+      if(!drop()) {
+        error("unknown thingy\n");
+      } else {
+        push("literal");
+        find();
+        add_to_definition();
+        add_to_definition();
+      }
     } else {
       to_number();
       if(!drop()) {
@@ -350,7 +319,7 @@ void interpret(void) {
     }
   }
 
-  printf(" ok\n");
+  out(" ok\n");
 };
 
 
@@ -359,34 +328,31 @@ int main (void) {
   sp = s0 = malloc(MAX_STACK);
 
   // define primitives
-  define_primitive("drop", &drop);
-  define_primitive("dup", &dup);
-  define_primitive("dup2", &dup2);
-  define_primitive(".s", &dot_s);
-  define_primitive("@", &fetch);
-  define_primitive("!", &store);
-  define_primitive("+", &plus);
+  define("drop", PRIMITIVE, &drop);
+  define("dup", PRIMITIVE, &dup);
+  define(".s", PRIMITIVE, &dot_s);
+  define("@", PRIMITIVE, &fetch);
+  define("!", PRIMITIVE, &store);
+  define("+", PRIMITIVE, &plus);
 
-  define_primitive(">number", &to_number);
-  define_primitive("word", &word);
-  define_primitive("string=", &string_eq);
+  define(">number", PRIMITIVE, &to_number);
+  define("word", PRIMITIVE, &word);
+  define("string=", PRIMITIVE, &string_eq);
 
-  define_primitive("if", &iff);
-  define_primitive("then", &then);
+  define("if", PRIMITIVE, &iff);
+  define("then", PRIMITIVE, &then);
 
-  define_primitive("find", &find);
-  define_primitive("literal", &literal);
-  define_primitive("constant", &constant);
-  define_primitive("variable", &variable);
-  define_primitive(":", &colon);
-  define_primitive(";", &semicolon); cp->type = IMMEDIATE;
-  define_primitive(",", &add_to_definition);
-  define_primitive("execute", &execute);
-  define_primitive("interpret", &interpret);
+  define("find", PRIMITIVE, &find);
+  define("literal", PRIMITIVE, &literal);
+  define("constant", PRIMITIVE, &constant);
+  define("variable", PRIMITIVE, &variable);
+  define(":", PRIMITIVE, &colon);
+  define(";", PRIMITIVE, &semicolon); cp->type = IMMEDIATE;
+  define(",", PRIMITIVE, &add_to_definition);
+  define("execute", PRIMITIVE, &execute);
+  define("interpret", PRIMITIVE, &interpret);
 
   while(*tib) {
     interpret();
   }
-
-  return 0;
 };
